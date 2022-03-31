2c7bb3d1253bf075ad3d28e2172b3f2b5acedfe0
Luis Felipe Fabiane <luisffabiane@gmail.com>
2022-03-31 10:35:23 -0300

    Middlewares checksExistsUserAccount
    Nesse commit foi criado o middlewares para verificar a existência de usuário

--- src/index.js
+++ src/index.js
@@ -10,7 +10,17 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(400).json({error: "User not found!"});
+  }
+
+  request.user = user;
+
+  return next;
 }
 
 app.post('/users', (request, response) => {
