24da09968545cb3eb339eb6bd638a800a0105558
Luis Felipe Fabiane <luisffabiane@gmail.com>
2022-03-31 10:30:55 -0300

    Criada rota post users
    Nesse commit foi adicionado o código da rota /users do tipo POST

--- src/index.js
+++ src/index.js
@@ -1,21 +1,39 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const userAlredyExists = users.some(
+    (user) => user.username === username
+  );
+
+  if(userAlredyExists) {
+    return response.status(400).json({error: "User Alredy Exists!"});
+  }
+
+  user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  }
+
+  users.push(user);
+
+  return response.status(200).json(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
