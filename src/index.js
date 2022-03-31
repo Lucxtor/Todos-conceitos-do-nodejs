fc22a233eb66bd704ce38f09674e1f6fdd2120a4
Luis Felipe Fabiane <luisffabiane@gmail.com>
2022-03-31 11:18:24 -0300

    Put e Patch Todo
    Criadas as rotas para atualizar uma tarefa e para marcar a tarefa como concluída pelo id

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function checksExistsUserAccount(request, response, next) {
   const user = users.find(user => user.username === username);
 
   if(!user) {
-    return response.status(400).json({error: "User not found!"});
+    return response.status(404).json({error: "User not found!"});
   }
 
   request.user = user;
@@ -70,11 +70,36 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+  const { title, deadline } = request.body;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo) {
+    return response.status(404).json({error: "Todo not Found!"});
+  }
+
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.status(201).send()
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+  const { title, deadline } = request.body;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo) {
+    return response.status(404).json({error: "Todo not Found!"});
+  }
+
+  todo.done = true;
+
+  return response.status(201).send()
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
